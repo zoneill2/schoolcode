e9cc85f32778a4a29c02fbb19fc4ed1139d249ce
zoneill2 <60304687+zoneill2@users.noreply.github.com>
2020-06-25 19:55:00 -0400

    Add files via upload

---
+++ Semester4/Clicker/Clicker/Form1.Designer.cs
@@ -0,0 +1,232 @@
+﻿namespace Clicker
+{
+    partial class Clicker
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.retryButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 10000;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.Blue;
+            this.button2.Image = global::Clicker.Properties.Resources.bg;
+            this.button2.Location = new System.Drawing.Point(532, 429);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(139, 47);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Start";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(366, 437);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 25);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Score";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 6;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
+            this.tableLayoutPanel1.Controls.Add(this.button1, 1, 1);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(10, 12);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 4;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(776, 400);
+            this.tableLayoutPanel1.TabIndex = 3;
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.AllowDrop = true;
+            this.trackBar1.LargeChange = 1;
+            this.trackBar1.Location = new System.Drawing.Point(20, 429);
+            this.trackBar1.Maximum = 2;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(288, 45);
+            this.trackBar1.TabIndex = 0;
+            this.trackBar1.TickStyle = System.Windows.Forms.TickStyle.Both;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Lime;
+            this.label2.Location = new System.Drawing.Point(13, 466);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 16);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Easy";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Yellow;
+            this.label3.Location = new System.Drawing.Point(135, 465);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(62, 16);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Medium";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.Red;
+            this.label4.Location = new System.Drawing.Point(272, 465);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(42, 16);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Hard";
+            // 
+            // retryButton
+            // 
+            this.retryButton.BackgroundImage = global::Clicker.Properties.Resources.restart;
+            this.retryButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.retryButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.retryButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.retryButton.ForeColor = System.Drawing.Color.Green;
+            this.retryButton.Location = new System.Drawing.Point(85, 100);
+            this.retryButton.Name = "retryButton";
+            this.retryButton.Size = new System.Drawing.Size(240, 222);
+            this.retryButton.TabIndex = 1;
+            this.retryButton.Text = "Retry";
+            this.retryButton.UseVisualStyleBackColor = true;
+            this.retryButton.Visible = false;
+            this.retryButton.Click += new System.EventHandler(this.retryButton_Click);
+            // 
+            // exitButton
+            // 
+            this.exitButton.BackgroundImage = global::Clicker.Properties.Resources.close;
+            this.exitButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.exitButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.exitButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.exitButton.ForeColor = System.Drawing.Color.Maroon;
+            this.exitButton.Location = new System.Drawing.Point(471, 99);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(240, 224);
+            this.exitButton.TabIndex = 4;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Visible = false;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(78)))), ((int)(((byte)(78)))), ((int)(((byte)(78)))));
+            this.button1.BackgroundImage = global::Clicker.Properties.Resources.target2;
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(132, 103);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(123, 94);
+            this.button1.TabIndex = 0;
+            this.button1.Text = " ";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Visible = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Clicker
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(78)))), ((int)(((byte)(78)))), ((int)(((byte)(78)))));
+            this.ClientSize = new System.Drawing.Size(798, 503);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.retryButton);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.MaximumSize = new System.Drawing.Size(814, 542);
+            this.MinimumSize = new System.Drawing.Size(814, 542);
+            this.Name = "Clicker";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Clicker";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Button retryButton;
+        private System.Windows.Forms.Button exitButton;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Semester4/Clicker/Clicker/Form1.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+using Clicker.Properties;
+
+namespace Clicker
+{
+    public partial class Clicker : Form
+    {
+        System.Timers.Timer t;
+        int s = 0;
+        int total = 0;
+        int endNum = 0;
+        int row = 0;
+        int col = 0;
+        int choice;
+        int time;
+        Random rand = new Random();
+        SoundPlayer splayer = new SoundPlayer(Resources.bell);
+        
+        public Clicker()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)  //on button press make invisible and play sound
+        {
+            splayer.Play();
+            button1.Visible = false;
+            //randSpot(col, row);
+            total++;
+           
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            choice = trackBar1.Value;       //trackbar decides difficulty, assigned to values
+            if(choice==0)
+            {
+                time = 700;
+            }
+            else if (choice == 1)
+            {
+                time = 500;
+            }
+            else
+            {
+                time = 350;
+            }
+
+            t = new System.Timers.Timer();      //creates new timer
+            t.Interval = time;
+            t.Elapsed += OnTimeEvent; //adds event method on each second elapsed
+
+            t.Start(); //start timer
+            label2.Visible = false;
+            label3.Visible = false;
+            label4.Visible = false;
+            button2.Visible = false;
+            trackBar1.Visible = false;
+        }
+
+        private void OnTimeEvent(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            Invoke(new Action(() => //on each second it will add one to s and end num
+            {           
+                s++;
+                endNum++;
+
+                if(s==1)
+                {
+                    if (endNum == 26) //ends game
+                    {
+                        label1.Text = total + "/" + (endNum-1);
+                        exitButton.Visible = true;
+                        retryButton.Visible = true;
+                        tableLayoutPanel1.Visible = false;
+                        t.Stop();
+                    }
+                    else
+                    {
+                        button1.Visible = false; 
+                        randSpot(col, row); //calls method to change button location
+                        s = 0;
+                        label1.Text = total + "/" + (endNum-1);                        
+                    }
+                
+                }
+            }));
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+            button1.BackColor = Color.White;
+
+        }
+
+        public void randSpot(int col, int row) //method assigns random position to button
+        {         
+
+            col = rand.Next(0, 6);
+            row = rand.Next(0, 5);
+
+            while(tableLayoutPanel1.GetRow(button1)==row)
+            {
+                row = rand.Next(0, 5);
+            }                                                        // these while loops dont allow same spot 2 times in a row
+
+            while (tableLayoutPanel1.GetColumn(button1) == col)
+            {
+                col = rand.Next(0, 6);
+            }
+            tableLayoutPanel1.SetRow(button1, row);
+            tableLayoutPanel1.SetColumn(button1, col);
+
+            button1.Visible = true;
+            t.Start(); //restarts timer
+        }
+
+        private void retryButton_Click(object sender, EventArgs e)
+        {
+            Application.Restart();
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+
+    }
+}
